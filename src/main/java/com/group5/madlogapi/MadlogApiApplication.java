46f5e54dbf4e2f559ef94100a54358019a1d66cc
Hasan <36068229+hasandk@users.noreply.github.com>
2021-10-02 23:01:22 +0200

    crud product works

---
+++ src/main/java/com/group5/madlogapi/MadlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.group5.madlogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadlogApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/group5/madlogapi/product/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.group5.madlogapi.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
